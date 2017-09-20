7ab52e76564f5167d688e5bccf44c7c0384853ea
MuteBard <csevere@brandeis.edu>
2017-09-20 01:31:17 -0400

    styling part 1

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,9 +1,27 @@
+//rather than creating a seperate CSS file as we do in web apps
+//we put all of our style relating to a component within the component file
+
+
+
 import React from 'react';
 //destructured import
 import {Text} from 'react-native';
 
+//Destructuring is used to reference our style
+const {textStyle} = styles;
+
+//Instead of using className = "class" in react, you use
+//style = {styleName} this is also called a property
 const Header = () =>{
   return (<Text>Albums!</Text>);
+};
+
+//CSS works differently within React Native. atm theres no bootstrap help (rip BS4)
+//is there flexbox?
+const styles = {
+  textStyle:{
+    fontSize: 70
+  }
 };
 
 export default Header;
