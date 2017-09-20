cead214bd1159814addd9b291ee1f23d8abf8a3e
MuteBard <csevere@brandeis.edu>
2017-09-20 01:09:36 -0400

    create simple tester with index file

---
+++ albums/index.ios.js
@@ -0,0 +1,12 @@
+//Index.ios.js - place code  in here for IOS
+
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+const App = () => {
+  return (
+    <Text>some text</Text>
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App); 
