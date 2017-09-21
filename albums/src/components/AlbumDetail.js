6f491859c8fc6495a0e209d6bad9b7a6ae41619b
MuteBard <csevere@brandeis.edu>
2017-09-20 22:09:29 -0400

    Card has styling

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import Card from './Card'; 
 
 const AlbumDetail = (props) => {
   console.log(props.album.title)
   return(
-    <View>
+    //card needs to be instructed on how to render any children that gets passed directly
+    //<Text>{props.album.title}</Text> becomes the props.childern
+    <Card>
       <Text>{props.album.title}</Text>
-    </View>
+    </Card>
   ); 
 };
 export default AlbumDetail;
