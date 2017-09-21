7e271da31896dda7285a248385fb46176a578e93
MuteBard <csevere@brandeis.edu>
2017-09-21 11:33:26 -0400

    using images in React Native

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import {Text, View } from 'react-native';
+import {Text, View, Image } from 'react-native';
 import Card from './Card'; 
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
-  console.log(props.album.title)
-  return(
+//DESTRUCTURING NOTES
+//Whenever we start to get more than two references to a prop
+//it always good practice to destructre that prop
+// so we scale from:
+//const AlbumDetail = (props) => {
+//to
+//const AlbumDetail = ({album}) => {
+//then we destructure album within the functional component body 
 
-    //add two new view tags, one that will house the image and 
-    //one that will house the text
+//IMAGES NOTE EXCLUSIVE TO REACT NATIVE
+//In react native, unlike images used in html, images do not expand to fill
+//their container by default, you must manually add in a styling rule that
+//tells the image tag exactly how big the image should be
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image} = album;
+  return(
     <Card>
       <CardSection>
         <View>
+          <Image 
+            style={styles.thumbnailStyle } 
+            source={{uri:thumbnail_image}}
+          />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -26,6 +40,10 @@ const styles = {
   headerContentStyle:{
     flexDirection : 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle:{
+    height : 50,
+    width : 50
   }
 };
 export default AlbumDetail;
