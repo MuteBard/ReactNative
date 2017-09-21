dbaff50c905291f44e6419e7d82705f3328a2ede
MuteBard <csevere@brandeis.edu>
2017-09-21 11:56:53 -0400

    using images part 2 spanning images across screen no matter size

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -3,47 +3,75 @@ import {Text, View, Image } from 'react-native';
 import Card from './Card'; 
 import CardSection from './CardSection'
 
-//DESTRUCTURING NOTES
-//Whenever we start to get more than two references to a prop
-//it always good practice to destructre that prop
-// so we scale from:
-//const AlbumDetail = (props) => {
-//to
-//const AlbumDetail = ({album}) => {
-//then we destructure album within the functional component body 
-
-//IMAGES NOTE EXCLUSIVE TO REACT NATIVE
-//In react native, unlike images used in html, images do not expand to fill
-//their container by default, you must manually add in a styling rule that
-//tells the image tag exactly how big the image should be
+//Set up some margin on the "div" that is surrounding the 
+//image 
 const AlbumDetail = ({album}) => {
-  const {title, artist, thumbnail_image} = album;
+  const {
+    title,
+    artist,
+    thumbnail_image,
+    image
+  } = album;
+  const {
+    thumbnailContainerStyle,
+    thumbnailStyle,
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+  
   return(
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image 
-            style={styles.thumbnailStyle } 
+            style={thumbnailStyle} 
             source={{uri:thumbnail_image}}
           />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle} 
+          source={{uri: image}}
+        />
       </CardSection>
     </Card>
   ); 
 };
+//In the imageStyles object, we want to make sure that
+//we can span the image from across the screen and this
+//can be a bit tricky. code for it is below
 
+//After that is done, we face an issue where we cant scroll
+//down :c
+//the remedy for this is in the next git commit
 const styles = {
   headerContentStyle:{
     flexDirection : 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle:{
+    fontSize:18
+  },
   thumbnailStyle:{
     height : 50,
     width : 50
+  },
+  thumbnailContainerStyle:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle:{
+    height: 300,
+    flex: 1,
+    width : null
   }
 };
 export default AlbumDetail;
