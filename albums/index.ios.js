24cc76465f01e69bdbdd3985320d148de1f8b27f
MuteBard <csevere@brandeis.edu>
2017-09-21 12:20:53 -0400

    solving scrolling and bounce back issues CORRECT VERSION

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -4,10 +4,13 @@ import {View, AppRegistry} from 'react-native';
 import Header from './src/components/Header'
 import AlbumList from './src/components/AlbumLists'
 
-//to return more than one component, you must wrap them in view tags
+//add the style in this root view
+// <View style={{flex: 1}}>*stuff*</View> means to 
+//please expand this component to fill the entire
+//content area of the device
 const App = () => {
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Header headerText={'Albums are food'}/>
       <AlbumList/>
     </View>
