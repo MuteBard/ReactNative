0220231ccf755f0e79666b950330bc4baec7fd64
MuteBard <csevere@brandeis.edu>
2017-09-24 02:35:58 -0400

    refactored text input part 2

--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -1,13 +1,5 @@
-/*
-whenever we have a file called index.js in a directory, whenever we try to
-import a file without specifying a file name, it will by default select the
-index.js file
-
-instead of importing the button then exporting the button, we will import out
-button this way. it works like a stepping stone
-*/
-
 export * from './Button';
 export * from './Header';
 export * from './Card';
 export * from './CardSection';
+export * from './Input';
