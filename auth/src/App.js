e6f4e9cc40a102818125d7d24bd8c8990cbc88ae
MuteBard <csevere@brandeis.edu>
2017-09-24 01:22:01 -0400

    firebase set up

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
 
+
 class App extends Component{
+
+  //lifecycle methods
+  componentWillMount(){
+    firebase.initalizeApp({
+      apiKey: 'AIzaSyCXHVAQdsZSgDdMPhT-q55dA3dQvFvRqlw',
+      authDomain: 'authentication-ac3a5.firebaseapp.com',
+      databaseURL: 'https://authentication-ac3a5.firebaseio.com',
+      projectId: 'authentication-ac3a5',
+      storageBucket: 'authentication-ac3a5.appspot.com',
+      messagingSenderId: '656601760367'
+    });
+  }
   render(){
     return(
       <View>
