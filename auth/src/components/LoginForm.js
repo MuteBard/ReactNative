d21f123cc74c6cf9aa31ae7e60d0e064dd6dc983
MuteBard <csevere@brandeis.edu>
2017-09-24 02:02:57 -0400

    unrefactored text input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection} from './common';
 
+
+/*
+Handling Text inputs is a bit of a pain in react Native
+we are first gonn implement a text input inside of the LoginForm
+then refactor it and put it into a separate component
+
+1) By Default, text inputs do not have a set height and width, they behave just
+like the image tags
+*/
 class LoginForm extends Component{
+  state = {text:''}
+
   render(){
     return(
       <Card>
         <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({text})}
+            style={{height:20, width:100}}>
+          </TextInput>
+
         </CardSection>
         <CardSection>
         </CardSection>
